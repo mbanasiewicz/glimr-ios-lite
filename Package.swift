757ac959c84aa9bdc9a4ca12e087d1c1553408a3
Maciej Banasiewicz <maciej.banasiewicz@schibsted.com>
2020-09-18 16:26:05 +0200

    Force dynamic

--- Package.swift
+++ Package.swift
@@ -10,7 +10,9 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GLGeoRealtime",
-            targets: ["GLGeoRealtime"])
+            type: .dynamic,
+            targets: ["GLGeoRealtime"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
---
+++ Source/Dummy.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maciej Banasiewicz on 2020-09-18.
+//
+
+import Foundation
